e99cd2b96079b323dda11956a893ae553be308b9
apoorva nayak <nayakapoorva2010@gmail.com>
2026-02-14 14:51:42 +0530

    /

--- src/app/(site)/cart/page.jsx
+++ src/app/(site)/cart/page.jsx
@@ -1,6 +1,7 @@
 import CartPage from "@/components/cart/CartPage";
 import CartSummary from "@/components/cart/CartSummary";
 import DeliveryInfo from "@/components/productdetail/DeliveryInfo";
+import RelatedProducts from "@/components/productdetail/RelatedProducts";
 
 export default function CartRoute() {
   return (
@@ -8,6 +9,7 @@ export default function CartRoute() {
       <CartPage />
       <DeliveryInfo />
       <CartSummary />
+      <RelatedProducts />
     </main>
   );
 }
